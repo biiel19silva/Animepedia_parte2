b26692e1ef24b843779e3c0e2f467e86b60af1a7
biiel19silva <161843915+biiel19silva@users.noreply.github.com>
2025-11-13 12:46:22 -0300

    Create equipe.php

---
+++ equipe.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animepedia</title>
+    <style type="text/css">
+        @import url("styles/style.css");
+    </style>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <?php include 'nav.php'; ?>
+    <main>
+        <h2>Equipe de Desenvolvimento desse Web Site</h2>
+        <!--ul significa unordered list(lista não ordenada)-->
+        <!--li significa list item, item de lista-->
+        <ul>
+            <li>Gabriel de Jesus Silva (matricula: 202302376924)</li>
+        </ul>
+    </main>
+    <?php include 'footer.php'; ?>
+    <script src="js/tema.js"></script>
+</body>
+</html>
