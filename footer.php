7da16cf0dd306cbda8e8dbd4ad9d936930ab2006
biiel19silva <161843915+biiel19silva@users.noreply.github.com>
2025-11-15 16:37:58 -0300

    Delete footer.php

--- footer.php
+++
@@ -1,4 +0,0 @@
-<footer class="rodapé">
-  <p>© 2025 Animepedia. Todos os direitos reservados.</p>
-  <p>Este projeto é uma ferramenta educacional e não comercial. As informações e imagens de animes são utilizadas para fins de demonstração, aprendizado e ilustração de conceitos de desenvolvimento web.</p>
-</footer>
